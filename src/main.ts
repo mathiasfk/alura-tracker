21e3c9672ddced35c4b429dca8203d84858e5aaa
Mathias Kriebel <mathias.kriebel@gmail.com>
2023-07-08 18:22:08 -0300

    Vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import '@fortawesome/fontawesome-free/css/all.css'
+import { inject } from '@vercel/analytics'
 import roteador from './roteador'
 import { key, store } from './store'
+ 
+inject();
 
 createApp(App)
 .use(roteador)
